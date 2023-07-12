4e822fce6c6def9c5062b70a8def3c74b33e7ce5
Alexandre Calaça <alexcalaca@gmail.com>
2023-07-11 22:25:57 -0300

    feat: compare word2 parameter

--- lib/merge_strings_alternately_1768_leet_code.rb
+++ lib/merge_strings_alternately_1768_leet_code.rb
@@ -7,6 +7,7 @@ class MergeStringsAlternately1768LeetCode
     merged = ""
     for i in 0...[word1.size, word2.size].max
       merged += word1[i] if i < word1.size
+      merged += word2[i] if i < word2.size
     end
   end
   
