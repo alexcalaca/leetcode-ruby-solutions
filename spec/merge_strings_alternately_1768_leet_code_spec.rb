f1ae12d9c4cc346adb25e3040a7beaedff66cb00
Alexandre Calaça <alexcalaca@gmail.com>
2023-07-11 22:33:14 -0300

    test: create rspec context 'when word1 is empty'

--- spec/merge_strings_alternately_1768_leet_code_spec.rb
+++ spec/merge_strings_alternately_1768_leet_code_spec.rb
@@ -12,4 +12,11 @@ describe MergeStringsAlternately1768LeetCode do
       expect(klass.merge_alternately(word1, word2)).to eq("a1b2c3")
     end    
   end
+  
+  context 'when word1 is empty' do
+    it 'returns the non-empty word as the empty string' do
+      expect(klass.merge_alternately("", "abcdef")).to eq("abcdef")
+      expect(klass.merge_alternately("", "123")).to eq("123")
+    end
+  end
 end
