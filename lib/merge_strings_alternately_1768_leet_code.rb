c1cfa1eb7167043d9f8c45452aff9bc1d1e9338f
Alexandre Calaça <alexcalaca@gmail.com>
2023-07-11 22:24:42 -0300

    feat: set up the loop

--- lib/merge_strings_alternately_1768_leet_code.rb
+++ lib/merge_strings_alternately_1768_leet_code.rb
@@ -5,6 +5,8 @@ class MergeStringsAlternately1768LeetCode
   # @return {String}
   def merge_alternately(word1, word2)
     merged = ""
+    for i in 0...[word1.size, word2.size].max
+    end
   end
   
 end
