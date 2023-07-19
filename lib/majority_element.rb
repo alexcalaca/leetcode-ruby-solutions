eccd90d84f158054659edb20a28dc991362ac207
Alexandre Calaça <alexcalaca@gmail.com>
2023-07-18 22:25:47 -0300

    feat: add initial blueprint

---
+++ lib/majority_element.rb
@@ -0,0 +1,10 @@
+# 169. Majority Element LeetCode
+
+# Given an array nums of size n, return the majority element.
+# The majority element is the element that appears more than ⌊n / 2⌋ times.
+# You may assume that the majority element always exists in the array.
+
+# @param {Integer[]} nums
+# @return {Integer}
+def majority_element(nums)
+end
