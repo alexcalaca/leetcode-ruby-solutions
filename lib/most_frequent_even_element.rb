626ce22925a5b5ef4184a2f3869e615e2ac2b437
Alexandre Calaça <alexcalaca@gmail.com>
2023-07-23 00:38:46 -0300

    feat: add initial blueprint for the most_frequent_even_element problem

---
+++ lib/most_frequent_even_element.rb
@@ -0,0 +1,8 @@
+# 2404. Most Frequent Even Element
+# Given an integer array nums, return the most frequent even element.
+# If there is a tie, return the smallest one. If there is no such element, return -1.
+
+# @param {Integer[]} nums
+# @return {Integer}
+def most_frequent_even(nums)
+end
