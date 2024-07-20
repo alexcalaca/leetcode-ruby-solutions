e95a1fbf71822f6c557706cd1fae496727272e5f
Alexandre Calaça <alexcalaca@gmail.com>
2024-07-20 02:17:06 -0300

    feat: add description to the  'Find the Child Who Has the Ball After K Seconds' 3178 leetcode 3178 challenge

--- lib/find_the_child_who_has_the_ball_after_k_seconds_3178.rb
+++ lib/find_the_child_who_has_the_ball_after_k_seconds_3178.rb
@@ -1,1 +1,14 @@
+#3178. Find the Child Who Has the Ball After K Seconds
+# https://leetcode.com/problems/find-the-child-who-has-the-ball-after-k-seconds/description/
+# You are given two positive integers n and k. There are n children numbered from 0 to n - 1
+# standing in a queue in order from left to right. Initially, child 0 holds a ball and the direction
+# of passing the ball is towards the right direction. After each second, the child holding the ball
+# passes it to the child next to them. Once the ball reaches either end of the line, i.e. child 0 or
+# child n - 1, the direction of passing is reversed.
+#Return the number of the child who receives the ball after k seconds.
 
+# @param {Integer} n
+# @param {Integer} k
+# @return {Integer}
+def number_of_child(n, k)
+end
