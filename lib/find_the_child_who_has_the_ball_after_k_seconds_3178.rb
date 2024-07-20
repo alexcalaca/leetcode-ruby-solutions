3a0a3626bdc4ba20bacd5004cca087db267be4c3
Alexandre Calaça <alexcalaca@gmail.com>
2024-07-20 02:18:44 -0300

    test: add rspec tests to the 'Find the Child Who Has the Ball After K Seconds' 3178 leetcode 3178 challenge

--- lib/find_the_child_who_has_the_ball_after_k_seconds_3178.rb
+++ lib/find_the_child_who_has_the_ball_after_k_seconds_3178.rb
@@ -20,3 +20,31 @@ def number_of_child(n, k)
     full_cycle - steps
   end
 end
+
+#test
+require 'rspec'
+
+RSpec.describe '#number_of_child' do
+  context "when steps >= n" do
+    it 'ball moves right and then reverses' do
+      expect(number_of_child(5, 14)).to eq(2)
+      expect(number_of_child(4, 10)).to eq(2)
+      expect(number_of_child(7, 15)).to eq(3)
+      expect(number_of_child(3, 7)).to eq(1)
+      expect(number_of_child(6, 20)).to eq(0)
+      expect(number_of_child(5, 8)).to eq(0)
+    end
+  end
+
+  context "when steps < n" do
+    it 'Ball is still moving right' do
+      expect(number_of_child(5, 1)).to eq(1)
+      expect(number_of_child(4, 3)).to eq(3)
+      expect(number_of_child(7, 6)).to eq(6)
+      expect(number_of_child(3, 2)).to eq(2)
+      expect(number_of_child(6, 4)).to eq(4)
+    end
+  end  
+end
+
+RSpec::Core::Runner.run([$__FILE__])
