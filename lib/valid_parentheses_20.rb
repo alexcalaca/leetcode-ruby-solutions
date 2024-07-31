2b68182eb45f62baa33aac97bb39f2a6b0202cf9
Alexandre Calaça <alexcalaca@gmail.com>
2024-07-31 00:30:18 -0300

    test: add rspec tests 'valid parentheses' leetcode 20 challenge

--- lib/valid_parentheses_20.rb
+++ lib/valid_parentheses_20.rb
@@ -18,3 +18,55 @@ def is_valid(s)
   end
   stack.empty?
 end
+
+# Test
+require 'rspec'
+
+RSpec.describe '#is_valid' do
+  context 'when open brackets are closed by the same type of brackets' do
+    it 'returns true' do
+      expect(is_valid("()")).to be_truthy
+    end
+
+    it 'returns false' do
+      expect(is_valid("(]")).to be_falsy
+    end
+  end
+
+  context 'when open brackets are closed in the correct order' do
+    it 'returns true' do
+      expect(is_valid("()[]")).to be_truthy
+    end
+
+    it 'returns false' do
+      expect(is_valid("([)]")).to be_falsey
+    end    
+  end
+
+  context 'when the order of closure respects the correct nesting' do
+    it 'returns true' do
+      expect(is_valid("([])")).to be_truthy
+    end
+
+    it 'returns true' do
+      expect(is_valid("[()]")).to be_truthy
+    end
+
+    it 'returns false' do
+      expect(is_valid("{[)]})")).to be_falsey
+    end
+  end
+
+  context 'when open bracket has a corresponding close bracket of the same type' do
+    it 'returns true' do
+      expect(is_valid("()[]{}")).to be_truthy
+    end
+  
+    it 'returns false' do
+      expect(is_valid("({[])")).to be_falsy
+    end
+  end  
+end
+
+RSpec::Core::Runner.run([$__FILE__])
+
