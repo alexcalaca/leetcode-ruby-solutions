8523b7bdccb5bef15440efeab5e7d1d087b55b89
Alexandre Calaça <alexcalaca@gmail.com>
2024-09-01 23:27:08 -0300

    feat: create file 'length_of_last_word_58'

---
+++ lib/length_of_last_word_58.rb
@@ -0,0 +1,10 @@
+#58. Length of Last Word
+# https://leetcode.com/problems/length-of-last-word/description/
+# Given a string s consisting of words and spaces, return the length of the last word in the string.
+# A word is a maximal  substring  consisting of non-space characters only.
+
+# @param {String} s
+# @return {Integer}
+def length_of_last_word(s)
+
+end
