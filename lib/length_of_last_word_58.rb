c9aa1d5024589ef429c1bf3de0e378051cd975fe
Alexandre Calaça <alexcalaca@gmail.com>
2024-09-01 23:33:28 -0300

    add test cases of the '58. Length of Last Word' leetcode challenge

--- lib/length_of_last_word_58.rb
+++ lib/length_of_last_word_58.rb
@@ -13,6 +13,29 @@ end
 require 'rspec'
 
 RSpec.describe '#length_of_last_word' do
+  it 'returns the length of the last word in "Hello World"' do
+    expect(length_of_last_word("Hello World")).to eq(5)
+  end
+
+  it 'returns the length of the last word in "   fly me   to   the moon  "' do
+    expect(length_of_last_word("   fly me   to   the moon  ")).to eq(4)
+  end
+
+  it 'returns the length of the last word in "luffy is still joyboy"' do
+    expect(length_of_last_word("luffy is still joyboy")).to eq(6)
+  end
+
+  it 'returns 0 for a string with only spaces "  "' do
+    expect(length_of_last_word("  ")).to eq(0)
+  end
+
+  it 'returns 0 for an empty string' do
+    expect(length_of_last_word("")).to eq(0)
+  end
+
+  it 'returns the length of the last word in "a "' do
+    expect(length_of_last_word("a ")).to eq(1)
+  end
 end
 
 RSpec::Core::Runner.run([$__FILE__])
